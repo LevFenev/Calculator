52287eb309358a03d881ccee2c7f8eea25fc4677
LevFenev <levfen@gmail.com>
2022-12-13 21:59:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/FourthFragment.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentFourthBinding
+
+class FourthFragment : Fragment() {
+
+    private var _binding: FragmentFourthBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!! // getter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+    // показывает в какую переменную будут складываться все связи с компонентами данного фрагмента
+        _binding = FragmentFourthBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /*binding.buttonFourthNext.setOnClickListener {
+            findNavController().navigate(R.id.action_FourthFragment_to_FirstFragment)
+        }*/
+
+        binding.buttonFourthPrev.setOnClickListener {
+            findNavController().navigate(R.id.action_FourthFragment_to_ThirdFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
